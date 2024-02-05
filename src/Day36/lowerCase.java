f05983e17ec16b6a5720b2b93a4f6398bd9a0d68
Dareddy Swathi <132138789+Swathi191-creator@users.noreply.github.com>
2024-02-05 17:47:38 +0530

    Add files via upload

---
+++ src/Day36/lowerCase.java
@@ -0,0 +1,19 @@
+package Day36;
+
+public class lowerCase {
+    public String toLowerCase(String s) {
+        //    StringBuilder result = new StringBuilder();
+
+        //     for (char c : s.toCharArray()) {
+        //         if (Character.isUpperCase(c)) {
+        //             result.append((char) (c + 32));
+        //         } else {
+        //             result.append(c);
+        //         }
+        //     }
+
+        //     return result.toString();
+        String str=s.toLowerCase();
+        return str;
+    }
+}
---
+++ src/Day36/stringHalves.java
@@ -0,0 +1,25 @@
+package Day36;
+
+public class stringHalves {
+    public boolean halvesAreAlike(String s) {
+        String a = s.substring(0, s.length() / 2);
+        String b = s.substring(s.length() / 2);
+        int countA = countVowels(a);
+        int countB = countVowels(b);
+        return countA == countB;
+    }
+    public int countVowels(String str) {
+        int count = 0;
+        for (char c : str.toCharArray()) {
+            if (isVowel(c)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public boolean isVowel(char c) {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
+                c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U';
+    }
+}
